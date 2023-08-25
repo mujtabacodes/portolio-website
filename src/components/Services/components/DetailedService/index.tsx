81eb1364bddfc2a704ce30425c263bc02a75cc8e
Mujtaba Shafique <mujiroy01@gmail.com>
2023-08-25 20:44:54 +0500

    DetailsService

--- src/components/Services/components/DetailedService/index.tsx
+++ src/components/Services/components/DetailedService/index.tsx
@@ -2,10 +2,18 @@ import './style.css'
 
 import React, { FC } from 'react'
 
-interface IComponent {}
+interface IComponent {
+	title: string
+	description: string
+}
 
-const DetailedService: FC<IComponent> = () => {
-	return <div></div>
+const DetailedService: FC<IComponent> = ({ title, description }) => {
+	return (
+		<div className='flex flex-row justify-between items-center border-b border-dashed border-slate-400 pb-10 pt-10 '>
+			<h1 className='text-4xl font-semibold'>{title}</h1>
+			<p className='w-1/2'>{description}</p>
+		</div>
+	)
 }
 
 export default DetailedService
--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -7,38 +7,68 @@ import digitalmarketingImg from '@assets/icons/growth.svg'
 import codingImg from '@assets/icons/programming-code-signs-1.svg'
 import Service from '@components/Services/components/Service'
 
+import DetailedService from './components/DetailedService'
+
 interface IServices {}
 
 const Services: FC<IServices> = () => {
 	return (
-		<div className='services-container mx-5 py-6 pl-20'>
-			<div className='service-text'>
-				<div
-					className='text-9xl font-bold relative text-gray-200'
-					style={{ fontSize: '10rem' }}
-				>
-					SER
-					<span className='text-black text-9xl absolute -top-3 -left-5'>SER</span>
+		<div className='flex flex-col py-10 mx-20'>
+			<div className='services-container '>
+				<div className='service-text'>
+					<div
+						className='text-9xl font-bold relative text-gray-200'
+						style={{ fontSize: '10rem' }}
+					>
+						SER
+						<span className='text-black text-9xl absolute -top-3 -left-5'>SER</span>
+					</div>
+					<div
+						className='text-9xl font-bold relative text-gray-200'
+						style={{ fontSize: '10rem' }}
+					>
+						VIC
+						<span className='text-black text-9xl absolute -top-3 -left-5'>VIC</span>
+					</div>
+					<div
+						className='text-9xl font-bold relative text-gray-200'
+						style={{ fontSize: '10rem' }}
+					>
+						ES<span className='text-black text-9xl absolute -top-3 -left-5'>ES</span>
+					</div>
 				</div>
-				<div
-					className='text-9xl font-bold relative text-gray-200'
-					style={{ fontSize: '10rem' }}
-				>
-					VIC
-					<span className='text-black text-9xl absolute -top-3 -left-5'>VIC</span>
-				</div>
-				<div
-					className='text-9xl font-bold relative text-gray-200'
-					style={{ fontSize: '10rem' }}
-				>
-					ES<span className='text-black text-9xl absolute -top-3 -left-5'>ES</span>
+				<div className='right-side grid grid-cols-2 gap-6 p-4'>
+					<Service image={codingImg} title='Website Development' />
+					<Service image={graphicImg} title='UI/UX Design' />
+					<Service image={codingImg} title='Mobile App Development' />
+					<Service image={digitalmarketingImg} title='Digital Marketing' />
 				</div>
 			</div>
-			<div className='right-side grid grid-cols-2 gap-6 p-4'>
-				<Service image={codingImg} title='Website Development' />
-				<Service image={graphicImg} title='UI/UX Design' />
-				<Service image={codingImg} title='Mobile App Development' />
-				<Service image={digitalmarketingImg} title='Digital Marketing' />
+			<div>
+				<DetailedService
+					title='Creative Solution'
+					description="Our team of experienced professionals is dedicated to delivering
+					exceptional results that exceed client expectations. Whether it's
+					branding, marketing, or web development,"
+				/>
+				<DetailedService
+					title='Design'
+					description="Our team of experienced professionals is dedicated to delivering
+					exceptional results that exceed client expectations. Whether it's
+					branding, marketing, or web development,"
+				/>
+				<DetailedService
+					title='Develop'
+					description="Our team of experienced professionals is dedicated to delivering
+					exceptional results that exceed client expectations. Whether it's
+					branding, marketing, or web development,"
+				/>
+				<DetailedService
+					title='Marketing'
+					description="Our team of experienced professionals is dedicated to delivering
+					exceptional results that exceed client expectations. Whether it's
+					branding, marketing, or web development,"
+				/>
 			</div>
 		</div>
 	)
