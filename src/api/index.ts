008e0644a7b7faae2c22173ef1a49a73ffd4b9cb
Mujtaba Shafique <mujiroy01@gmail.com>
2024-01-11 14:18:45 +0500

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,3 @@
 // export const baseURL = 'http://localhost:3003'
-export const baseURL = 'https://porfolio-website-moosh-4fc8d842f217.herokuapp.com'
+//export const baseURL = 'https://porfolio-website-moosh-4fc8d842f217.herokuapp.com'
+export const baseURL = 'https://portfolio-website-backend-sandy.vercel.app/'
